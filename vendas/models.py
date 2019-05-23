7e905ade589b1c7e955d5ec301842af49f4a2ed9
unknown <Murilo Rodrigues@DESKTOP-ONT7ENB.EDGE-BR.LOCAL>
2019-05-23 17:42:28 -0300

    go

--- vendas/models.py
+++ vendas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Empresa(models.Model):
+
+    Status_CHOICES =[('Ativo','Ativo'),('Desativado','Desativado')]
+    Nome = models.CharField(max_length=150)
+    Tipo = models.CharField(choices=Status_CHOICES,default='Ativo',max_length=50)
+
+    def __str__(self):
+        return self.Nome
+
+
